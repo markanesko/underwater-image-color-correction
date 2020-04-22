580256e28c9612fb983224b8382bb8db962a29bc
Marko Beric <marko.beric@doob3dlabs.com>
2020-04-22 16:07:09 +0200

    updates main.py

---
+++ main.py
@@ -0,0 +1,136 @@
+import os
+import numpy as np
+import cv2
+
+from matplotlib import pyplot as plt
+
+root = os.getcwd()
+data = os.path.join(root, 'data')
+
+img = os.path.join(data, 'image.jpg')
+
+CHANNEL_NUM = 3
+MIN_AVERAGE_RED = 156
+MAX_HUE_SHIFT = 120
+
+def calculate_average_color(im):
+    channel_sum = np.zeros(CHANNEL_NUM)
+    pixel_num = 0
+
+    im = im/255.0
+
+    pixel_num += (im.size/CHANNEL_NUM)
+    channel_sum += np.sum(im, axis=(0, 1))
+
+    bgr_mean = channel_sum / pixel_num
+
+    rgb_mean = list(bgr_mean)[::-1]
+
+    return np.matrix([[rgb_mean[0]],[rgb_mean[1]],[rgb_mean[2]]])
+
+def calculate_standard_deviation(im):
+    channel_sum = np.zeros(CHANNEL_NUM)
+    channel_sum_squared = np.zeros(CHANNEL_NUM)
+    pixel_num = 0
+
+    im = im/255.0
+
+    pixel_num += (im.size/CHANNEL_NUM)
+
+    channel_sum_squared += np.sum(np.square(im), axis=(0, 1))
+    bgr_mean = channel_sum / pixel_num
+
+    bgr_std = np.sqrt(channel_sum_squared / pixel_num - np.square(bgr_mean))
+
+    rgb_std = list(bgr_std)[::-1]
+
+    return rgb_std
+
+def hue_shift_red(rgb_vector, h):
+    U = np.cos(h * np.pi / 180)
+    W = np.sin(h * np.pi / 180)
+
+    hue_vector = np.matrix([[1], [U], [W]])
+    # rgb_vector = np.matrix([[r], [g], [b]])
+
+    params_matrix = np.matrix('0.299 0.701 0.168; 0.587 -0.587 0.330; 0.114 -0.144 -0.497')
+
+    params_hue_vector = np.matmul(params_matrix, hue_vector)
+
+    # A[0,:][:,0]
+
+    # a = params_hue_vector[2, :][0, :]
+
+    rgb_vector = np.multiply(params_hue_vector, rgb_vector)
+
+    return rgb_vector
+
+def calculate_hue_shift(average_vector):
+
+    new_average_red = average_vector[0, :][0, :].item()
+    hue_shift = 0
+
+    # print(new_average_red)
+    while new_average_red < MIN_AVERAGE_RED:
+        shifted_rgb_vector = hue_shift_red(average_vector, hue_shift)
+        hue_shift += 1
+        new_average_red = np.sum(shifted_rgb_vector)
+
+        # add max iteration check
+
+        # print(new_average_red)
+
+    return hue_shift
+
+def create_histogram():
+
+    return 0
+
+def normalizing_interval(normaliznig_array):
+    low, high = 0
+
+    return low, high
+
+def main():
+    im = cv2.imread(img) # image in M*N*CHANNEL_NUM shape, channel in BGR order
+
+    # memi = None
+    #
+    # for i, mem in enumerate(im):
+    #     if i == 2:
+    #         break
+    #     # print(mem)
+    #     # print(type(mem))
+    #     # print('\n')
+    #     memi = mem
+    #
+    # for j in memi:
+    #     print(j)
+    #
+    # return 0
+
+    for i, col in enumerate(['b', 'g', 'r']):
+        hist = cv2.calcHist([im], [i], None, [256], [0, 256])
+        for i, j in enumerate(hist):
+            print(i, j/255)
+
+        plt.plot(hist,color = col)
+        plt.xlim([0,256])
+
+        # print(type(hist))
+    plt.show()
+    average_vector = calculate_average_color(im)
+
+    print(average_vector)
+
+    hue_shift = calculate_hue_shift(average_vector * 255)
+
+    print(hue_shift)
+
+    # rgb_shift_vector = hue_shift_red(average_vector, 0)
+
+    # print(average_vector, rgb_shift_vector)
+    return 0
+
+
+main()
