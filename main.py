4379b0a0f4a08310bc8ea42e9ae34e4d9d75b757
Marko Beric <marko.beric@doob3dlabs.com>
2020-04-23 15:27:01 +0200

    implements normalize

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ img = os.path.join(data, 'image.jpg')
 CHANNEL_NUM = 3
 MIN_AVERAGE_RED = 156
 MAX_HUE_SHIFT = 120
+THRESHOLD_RATIO = 2000
 
 def calculate_average_color(im):
     channel_sum = np.zeros(CHANNEL_NUM)
@@ -86,13 +87,56 @@ def create_histogram():
 
     return 0
 
-def normalizing_interval(normaliznig_array):
-    low, high = 0
+def prepare_normalizing_hists(num_pixels, hists):
+    threshold_level = num_pixels / THRESHOLD_RATIO
+
+
+    norm_hist = []
+    norm_hist_r = []
+    norm_hist_g = []
+    norm_hist_b = []
+
+
+    norm_hist_r.append(0)
+    norm_hist_g.append(0)
+    norm_hist_b.append(0)
+
+    for i in range(0, 256):
+        if hists[0][i] - threshold_level < 2:
+            norm_hist_r.append(i)
+        if hists[1][i] - threshold_level < 2:
+            norm_hist_g.append(i)
+        if hists[2][i] - threshold_level < 2:
+            norm_hist_b.append(i)
+
+    norm_hist_r.append(255)
+    norm_hist_g.append(255)
+    norm_hist_b.append(255)
+
+    norm_hist.append(norm_hist_r)
+    norm_hist.append(norm_hist_g)
+    norm_hist.append(norm_hist_b)
+
+    return norm_hist
+
+def normalizing_interval(hist):
+    low = 0
+    high = 255
+    max_distance = 0
+
+    for i in range(1, len(hist)):
+        distance = hist[i] - hist[i-1]
+        if distance > max_distance:
+            max_distance = distance
+            high = hist[i]
+            low = hist[i-1]
 
     return low, high
 
 def main():
     im = cv2.imread(img) # image in M*N*CHANNEL_NUM shape, channel in BGR order
+    rows, cols, channels = im.shape
+    num_pixels = rows * cols
 
     # memi = None
     #
@@ -108,26 +152,46 @@ def main():
     #     print(j)
     #
     # return 0
+    hb, hg, hr = None, None, None
 
     for i, col in enumerate(['b', 'g', 'r']):
         hist = cv2.calcHist([im], [i], None, [256], [0, 256])
+        if col == 'b':
+            hb = hist
+        elif col == 'g':
+            hg = hist
+        else:
+            hr = hist
+
         for i, j in enumerate(hist):
-            print(i, j/255)
+            print(i, j)
 
         plt.plot(hist,color = col)
         plt.xlim([0,256])
 
         # print(type(hist))
     plt.show()
+
+    hists = []
+    hists.append(hr)
+    hists.append(hg)
+    hists.append(hb)
+
+    norm_hists = prepare_normalizing_hists(rows*cols, hists)
+
+    low, high = normalizing_interval(norm_hists[0])
+
+    print(low, high)
+
+    # print(hb, hg, hr)
+
     average_vector = calculate_average_color(im)
 
-    print(average_vector)
 
     hue_shift = calculate_hue_shift(average_vector * 255)
 
-    print(hue_shift)
-
     # rgb_shift_vector = hue_shift_red(average_vector, 0)
+
 
     # print(average_vector, rgb_shift_vector)
     return 0
